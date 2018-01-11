b325589fc81586c8220ad8591e11cd61966f2e17
Oswaldo <oswaldo7365@hotmail.com>
2018-01-10 23:55:39 -0400

    acomodando coordinador hash

--- CoordinadorPeerToPeer/src/Dominio/Usuario.java
+++ CoordinadorPeerToPeer/src/Dominio/Usuario.java
@@ -57,9 +57,8 @@ public class Usuario {
      * @param clave clave a convertir
      * @return la clave convertida
      */
-    private Integer toHash(String ip){
-        Integer hash = 512;
-        hash =  37*hash + ip.hashCode();
-        return hash;
-    }
+      static Integer toHash(String str){
+   int strHashCode = str.hashCode() % 100;
+   return strHashCode;
+}
 }
